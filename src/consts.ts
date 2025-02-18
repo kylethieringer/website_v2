6a861620cc714a0bd99ecd3b403efcc723bb0a9f
kylethieringer <kylethieringer@gmail.com>
2025-02-17 22:57:44 -0800

    add cname

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://www.kylethieringer.com";
+export const SITE_URL = "https://kylethieringer.com";
 export const SITE_TITLE = "kyle thieringer";
 export const SITE_DESCRIPTION =
   "neuro grad student at the university of washington";
